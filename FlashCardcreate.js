b234ee0ead17ac88df113f895c5fc60bbffc0d39
VelazquezquezLuis <35620778+VelazquezLuis@users.noreply.github.com>
2018-06-21 21:44:47 -0400

    change pics paths

--- FlashCardcreate.js
+++ FlashCardcreate.js
@@ -1,1 +1,66 @@
-console.log('helo')
+
+
+var nTitle = document.getElementById('tTitle1').value;
+
+document.getElementById('mySecondTextArea').value = nTitle;
+
+
+
+
+
+// var nTitle = document.querySelector('#tTitle1').value;
+//   nTitle.addEventListener('submit', () => {
+//     var title = new nTitle();
+//     document.querySelector('#frontP').innerHTML = title;
+// })
+
+
+
+
+//   formElement.addEventListener('submit', () => {
+//     var title = new nTitle();
+//     alert('thank you, ' + title.nTitle);
+// })
+
+
+
+
+
+
+
+// document.getElementById("myBtn").addEventListener("click", function(){
+//   document.getElementById("demo").innerHTML = "Hello World";
+// });
+
+
+
+
+// (function () {
+//   'use strict';
+
+//   function Person () {
+//     this.name = document.querySelector('#name').value;
+//     this.email = document.querySelector('#email').value;
+//     this.phone = document.querySelector('#phone').value;
+//     this.description = document.querySelector('#description').value;
+//   }
+
+//   var submitButton = document.querySelector('button[type=submit]');
+//   var formElement = document.querySelector('form');
+//   var formElement1 = document.forms[0];
+
+//   formElement.addEventListener('submit', () => {
+//     var p = new Person();
+//     alert('thank you, ' + p.name);
+//   })
+
+//   submitButton.addEventListener('click', (fred) => {
+//     //fred.stopPropagation();
+//     fred.preventDefault();
+//     var p = new Person();
+//     console.log(p.name);
+//   });
+
+// }());
+
+
